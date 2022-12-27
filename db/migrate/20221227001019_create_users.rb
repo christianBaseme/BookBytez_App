46bae698af17b6d0885a83e03a8715034febddc1
christianBaseme <cbaseme@uiowa.edu>
2022-12-26 18:24:49 -0600

    added the user model which has role for student and instructor

---
+++ db/migrate/20221227001019_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :course
+      t.string :email
+      t.string :major
+      t.string :role
+      t.integer :followers
+      t.integer :following
+
+      t.timestamps
+    end
+  end
+end
